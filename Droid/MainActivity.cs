7d4545a4bcf42eb2df23bbb23706fab2f7393ce6
samirgc <samirgc@DESKTOP-V1EFT37>
2018-05-16 23:02:51 +0530

    Add project files.

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Lottie.Forms.Droid;
+
+namespace Tabs.Droid
+{
+    [Activity(Label = "Tabs.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            AnimationViewRenderer.Init();
+
+            LoadApplication(new App());
+        }
+    }
+}
